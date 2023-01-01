27551d0dd521ef9120ebde0e5b3b4e95a2c74f46
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2023-01-01 15:25:55 +0700

    Create columns1.php

---
+++ columns1.php
@@ -0,0 +1,110 @@
+<!-- START FOOTER -->
+
+<footer id="footer">
+	
+	<div class="container"> <!-- 1152 pixels Container -->
+	
+		<!-- Contact Details -->
+		<div class="one-third column" id="contact-details">
+			<div>
+				<p>Connect with Us</p>
+				<h2 class="phone-number"><?php echo($dd['mobile']) ?></h2>
+				<a href="mailto:<?php echo($dd['email']) ?>"><?php echo($dd['email']) ?></a>
+			</div>
+		</div>
+		
+		<div class="two-thirds column" id="footer-links">
+			
+			<!-- Social Icons -->
+			<ul class="social-icons clearfix">
+				<li class="twitter"><a href="<?php echo($dd['twitter']) ?>">Twitter</a></li>
+				<li class="facebook"><a href="<?php echo($dd['facebook']) ?>">Facebook</a></li>
+				<li class="google-plus"><a href="http://www.bonekslot.net/">Google+</a></li>
+				<li class="linkedin"><a href="http://www.bonekslot.org/">LinkedIn</a></li>
+			</ul>
+			
+			<!-- Footer Navigation -->
+			<ul class="footer-nav clearfix">
+				<li><a href="home">Home</a></li>
+				<li><a href="editorialboard">Editorial Board</a></li>
+				<li><a href="contact">Contact</a></li>
+				<li><a href="blogs">Blog</a></li>
+				<li><a href="myaccount">Submit Article</a></li>
+			</ul>
+			
+			<div class="clear"></div> <!-- IE7 margin-bottom fix -->
+			
+		</div>
+		
+	</div>
+
+</footer>
+
+<footer id="footer-bottom">
+	
+	<div class="container"> <!-- 1152 pixels Container -->
+		<div class="sixteen columns">
+			
+			<div class="twelve columns alpha">
+				<ul class="links clearfix">
+					<li>&copy; <?php echo(date('Y')); ?> - <?php echo($dd['sitename']); ?><span>|</span></li>
+					<li><a href="http://www.bonekslot.org/">Designed BonekSlot ICT</a>
+				</ul>
+			</div>
+				
+			<div class="four columns omega">
+			</div>
+			
+		</div>
+	</div>
+	
+</footer>
+
+<!-- END FOOTER -->
+
+
+</div>
+
+<!-- Java Script -->
+<script src="js/respond.min.js"></script>
+<script src="js/selectnav.min.js"></script>
+<script src="js/jquery.min.html"></script>
+<script src="js/detectmobilebrowser.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.fitvids.min.js"></script>
+<script src="js/jquery.prettyPhoto.min.js"></script>
+<script src="js/jquery.flexslider.min.js"></script>
+<script src="js/jquery.carousel.min.js"></script>
+<script src="js/jquery.tweet.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/switcher.js"></script> <!-- Style Switcher -->
+
+<div style="display:none;">
+<p><a title="Slot Pulsa" href="http://cc87912-drupal-7.tw1.ru/public/journals/slot-pulsa/" rel="dofollow">http://cc87912-drupal-7.tw1.ru/public/journals/slot-pulsa/</a></p>
+<p><a title="Slot Online" href="http://revistaeolo.fconvida.org/templates/images/slot-online/" rel="dofollow">http://revistaeolo.fconvida.org/templates/images/slot-online/</a></p>
+<p><a title="Slot Gacor 2022" href="http://revista.ispsn.org/ojs/slot-gacor/" rel="dofollow">http://revista.ispsn.org/ojs/slot-gacor/</a></p>
+<p><a title="Slot Deposit Pulsa" href="https://prosecutioncmms.kp.gov.pk/slot-pulsa/" rel="dofollow">https://prosecutioncmms.kp.gov.pk/slot-pulsa/</a></p>
+<p><a title="Slot Online Terpercaya" href="http://interactome.dfci.harvard.edu/fragdb/cgi-bin/judi-slot-online/" rel="dofollow">http://interactome.dfci.harvard.edu/fragdb/cgi-bin/judi-slot-online/</a></p>
+<p><a title="Rtp Slot" href="https://lolly.comc.co.il/" rel="dofollow">https://lolly.comc.co.il/</a></p>
+<p><a title="Slot Demo" href="https://wp01.fpe.zcu.cz/wp-includes/slot-demo/" rel="dofollow">https://wp01.fpe.zcu.cz/wp-includes/slot-demo/</a></p>
+<p><a title="Slot Gacor Hari Ini" href="https://ciae.icar.gov.in/vendor/-/slot-gacor/" rel="dofollow">https://ciae.icar.gov.in/vendor/-/slot-gacor/</a></p>
+<p><a title="situs slot gacor" href="https://trang.lib.ru.ac.th/slot-gacor/" rel="dofollow">https://trang.lib.ru.ac.th/slot-gacor/</a></p>
+<p><a title="jam rtp slot" href="https://careers.mahidol.ac.th/rtp-slot/" rel="dofollow">https://careers.mahidol.ac.th/rtp-slot/</a></p>
+<p><a title="Slot Pulsa Tanpa Potongan" href="https://www.jaotipe.hku.hk/slot/" rel="dofollow">https://www.jaotipe.hku.hk/slot/</a></p>
+<p><a title="Link Slot Gacor" href="https://novosite.fsh.edu.br/wp-includes/assets/slot-gacor/" rel="dofollow">https://novosite.fsh.edu.br/wp-includes/assets/slot-gacor/</a></p>
+<p><a title="bocoran slot gacor hari ini" href="https://nip.qau.edu.pk/wp/.tmb/slot-gacor/" rel="dofollow">https://nip.qau.edu.pk/wp/.tmb/slot-gacor/</a></p>
+<p><a title="slot via pulsa" href="http://adversus.org/revista/files/journals/slot-pulsa/" rel="dofollow">http://adversus.org/revista/files/journals/slot-pulsa/</a></p>
+<p><a title="slot gacor 4d" href="https://career.perumnas.co.id/assets/4d/" rel="dofollow">https://career.perumnas.co.id/assets/4d/</a></p>
+<p><a title="slot dana" href="https://rumah.perumnas.co.id/template/slot-dana/" rel="dofollow">https://rumah.perumnas.co.id/template/slot-dana/</a></p>
+<p><a title="slot gacor" href="https://perumnas.co.id/ninja-slot-88-gacor-4d/" rel="dofollow">https://perumnas.co.id/ninja-slot-88-gacor-4d/</a></p>
+<p><a title="Slot Gacor Gampang Menang" href="https://www.shhorms.hku.hk/wp-includes/link-gacor/" rel="dofollow">https://www.shhorms.hku.hk/wp-includes/link-gacor/</a></p>
+<p><a title="Bocoran Slot Gacor" href="https://today.albion.edu/wp-content/uploads/2022/12/" rel="dofollow">https://today.albion.edu/wp-content/uploads/2022/12/</a></p>
+<p><a title="daftar slot pulsa" href="https://upincubator.upi.edu/wp-content/uploads/slot-deposit-pulsa-tanpa-potongan/" rel="dofollow">https://upincubator.upi.edu/wp-content/uploads/slot-deposit-pulsa-tanpa-potongan/</a></p>
+<p><a title="daftar slot gacor" href="https://survey.ticmi.co.id/upload/plugins/slot-pulsa/" rel="dofollow">https://survey.ticmi.co.id/upload/plugins/slot-pulsa/</a></p>
+<p><a title="cek rtp slot pragmatic live" href="https://thai.hku.hk/news/rtp-slot/" rel="dofollow">https://thai.hku.hk/news/rtp-slot/</a></p>
+<p><a title="cek rtp slot pragmatic live" href="https://scape.edu.hku.hk/wp-content/ngg/gacor/" rel="dofollow">https://scape.edu.hku.hk/wp-content/ngg/gacor/</a></p>
+<p><a title="rtp slot" href="http://dezvoltare.uvt.ro/sport/wp-includes/rtp/" rel="dofollow">http://dezvoltare.uvt.ro/sport/wp-includes/rtp/</a></p>
+
+</div>
+</body>
+</html>
